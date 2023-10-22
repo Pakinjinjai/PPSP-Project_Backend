c56b8511ca452b74b79950b830abd0354cbf0879
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-22 10:56:09 +0700

    Update health.js

--- schemas/health.js
+++ schemas/health.js
@@ -1,26 +1,29 @@
-const {Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const healthSchema = new Schema ({
-    temperature:{
-        type: Number,
-        default:null,
+const healthSchema = new Schema(
+  {
+    temperature: {
+      type: Number,
+      default: null,
     },
-    bloodPressure:{
-        type: Number,
-        default:null,
+    bloodPressure: {
+      type: Number,
+      default: null,
     },
-    heartRate:{
-        type: Number,
-        default:null,
+    heartRate: {
+      type: Number,
+      default: null,
     },
-    oxygen:{
-        type: Number,
-        default:null,
+    oxygen: {
+      type: Number,
+      default: null,
     },
-    userId:{
-        type:String,
-        required:true
-    }
-}, {timestamps:true});
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = model('health',healthSchema); 
+module.exports = model("health", healthSchema);
