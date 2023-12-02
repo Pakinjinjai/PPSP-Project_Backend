d52da0d1b75aca01a036f570ba7bd4696647213e
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-12-02 16:34:48 +0700

    Broker

--- broker.js
+++ broker.js
@@ -6,10 +6,14 @@ const mqtt = require("net").createServer(broker.handle);
 const wsServer = http.createServer();
 websocket.createServer({ server: wsServer }, broker.handle);
 
-wsServer.listen(8883, () => {
-  console.log("Broker MQTT running on port 8883");
+mqtt.on("client", (client) => {
+  console.log(client.id);
 });
 
 mqtt.listen(1883, () => {
   console.log("Broker MQTT running on port 1883");
+});
+
+wsServer.listen(8883, () => {
+  console.log("Broker MQTT running on port 8883");
 });
