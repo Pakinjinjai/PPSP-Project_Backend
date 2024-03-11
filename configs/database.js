45e2026691705a3c8e38378664eb60e22608185e
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-03-11 17:32:02 +0700

    Update

--- configs/database.js
+++ configs/database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const _init = async () => {
   mongoose
+    // .connect("mongodb://localhost:27017/Healthcare_WebApp")
     .connect("mongodb+srv://admin:1234@cluster0.up8drkr.mongodb.net/Serves")
     .then((v) => {
       console.log("Connected to MongoDB");
