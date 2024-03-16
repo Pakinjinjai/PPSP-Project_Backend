96c3d62d4f73497c7c3bdd5ca5f0c2fa48917baa
PAKIN JINJAI <pakin.281244@gmail.com>
2024-03-16 13:56:52 +0700

    Update to Deploy

--- configs/database.js
+++ configs/database.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+const URI = process.env.MONGO_URI
 
 const _init = async () => {
   mongoose
     // .connect("mongodb://localhost:27017/Healthcare_WebApp")
-    .connect("mongodb+srv://admin:1234@cluster0.up8drkr.mongodb.net/Servers")
+    .connect(URI)
     .then((v) => {
       console.log("Connected to MongoDB");
     })
