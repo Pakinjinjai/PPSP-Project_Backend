cd36baef7da62b6e5bc934d5d880aebe85c26062
PAKIN JINJAI <pakin.281244@gmail.com>
2024-03-24 13:05:35 +0700

    edit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const main = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
-  const port = 3000;
+  const port = process.env.PORT || 3000;
 
 
   routes(app);
