2432c8964b1e64a7ad2c20b0c73e4de9278ef48b
PAKIN JINJAI <63201161@kmitl.ac.th>
2024-04-28 14:26:01 +0700

    Update LineReply

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,12 @@ const userRoute = require("./users");
 const healthRoute = require("./health");
 const queueRoute = require("./queue");
 const replyRoute = require("./reply");
+const lineRoute = require("./linereply");
 
 module.exports = (app) => {
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/healths", healthRoute);
   app.use("/api/v1/queues", queueRoute);
   app.use("/api/v1/reply", replyRoute);
+  app.use("/api/v1/line", lineRoute);
 };
